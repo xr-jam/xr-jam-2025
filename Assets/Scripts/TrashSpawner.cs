5121894df1d12b7da541845cdaefa4dca218a76c
xr-jam <xr-jam@A65-23.etd-l.esiea-ouest.fr>
2025-01-18 15:30:41 +0100

    trash detection

--- Assets/Scripts/TrashSpawner.cs
+++ Assets/Scripts/TrashSpawner.cs
@@ -6,15 +6,37 @@ public class TrashSpawner : MonoBehaviour
 {
     [SerializeField]
     List<GameObject> trashPrefabs;
+
+
     public void SpawnRandom()
     {
         int rnd = Random.Range(0, trashPrefabs.Count);
         var obj = Instantiate(trashPrefabs[rnd], transform.position, Quaternion.identity, transform);
 
-        AddForce movement = obj.AddComponent<AddForce>();
+        Trash trash = obj.AddComponent<Trash>();
+        
+        //parse by name
+        if (obj.name.Contains("plastic"))
+        {
+            trash.Category = TrashType.Category.Plastic;
+        }
+        else if (obj.name.Contains("paper"))
+        {
+            trash.Category = TrashType.Category.Paper;
+        }
+        else if (obj.name.Contains("misc"))
+        {
+            trash.Category = TrashType.Category.Misc;
+        }
+        else if (obj.name.Contains("glass"))
+        {
+            trash.Category = TrashType.Category.Glass;
+        }
+
         obj.AddComponent<BoxCollider>();
         obj.AddComponent<Rigidbody>();
 
+        AddForce movement = obj.AddComponent<AddForce>();
         movement.surfaceSpeed = 1;
     }
 }
