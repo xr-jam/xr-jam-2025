a1e67d66f475009628ffe809ba13d12e5d9da3fa
Liffim <103305619+Liffim@users.noreply.github.com>
2025-01-19 15:41:18 +0100

    button

--- Assets/Scripts/HpManager.cs
+++ Assets/Scripts/HpManager.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class HpManager : MonoBehaviour
 {
     [SerializeField]
     GameObject iconParent;
+
+    [SerializeField]
+    GameObject button;
+
+    [SerializeField]
+    TrashManager manager;
 
     public void SubtractHp()
     {
@@ -16,12 +23,23 @@ public class HpManager : MonoBehaviour
 
             RawImage image = child.GetComponent<RawImage>();
                 
-            if (image.color == Color.white)
+            if (image && image.color == Color.white)
             {
                 image.color = Color.black;
+                if(i <= 0)
+                {
+                    button.active = true;
+                    //Button b = button.GetComponent<Button>();
+                    
+                    manager.timeToSpawn = 9999999;
+                }
                 break;
             }
         }
+    }
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
 
